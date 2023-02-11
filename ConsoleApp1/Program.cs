0e5033be950fa79be152b72f91052ba5d5301d7f
Giovanni <Giovanni@DESKTOP-86A1CAO>
2023-02-11 19:35:51 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Security.AccessControl;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIGITE SUA SENHA");
+
+            int senha = int.Parse(Console.ReadLine());
+
+            while(senha != 2002)
+            {
+                Console.WriteLine("Senha invalida");
+                
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Senha correta");
+            Console.ReadKey();
+        }
+
+   
+        
+        
+        
+        
+        
+
+        
+    }
+}
